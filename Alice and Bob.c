2da5a995fb5cc07458599854bbd53d70bdd100e1
HITFishily <guoyutong0828@icloud.com>
2016-05-06 18:42:31 +0800

    HOJ-2267

---
+++ Alice and Bob.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    while(a != 0 && b != 0){
+        printf("%d\n",a*3 - b);
+        scanf("%d %d",&a,&b);
+    }
+}
